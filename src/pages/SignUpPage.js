0129571b3d64fd21a2fdedce920dc5face6cc3f3
Pedro <pedropiacentini@usp.br>
2023-04-23 17:58:36 -0300

    cadastro

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,23 +1,78 @@
 import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import { signUp } from "../services/apiAuth";
 
 export default function SignUpPage() {
   const navigate = useNavigate();
 
+  const [form, setForm] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  function handleForm(e) {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
   function handleSignUp(e) {
     e.preventDefault();
-    navigate("/");
+    if (form.password !== form.confirmPassword) {
+      alert("Senha não é compatível com confirmar senha");
+      return;
+    }
+    delete form.confirmPassword;
+
+    signUp(form)
+      .then(res => {
+        console.log(res)
+        navigate("/");
+      })
+      .catch(err => {
+        alert(err.response.data);
+      });
+
   }
   return (
     <SingUpContainer>
       <form onSubmit={handleSignUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          name="userName"
+          type="text"
+          required
+          value={form.name}
+          onChange={handleForm}
+        />
+        <input
+          name="email"
+          placeholder="E-mail"
+          type="email"
+          required
+          value={form.email}
+          onChange={handleForm}
+        />
+        <input
+          name="password"
+          placeholder="Senha"
+          type="password"
+          required
+          value={form.password}
+          onChange={handleForm}
+        />
+        <input
+          name="confirmPassword"
+          placeholder="Confirme a senha"
+          type="password"
+          required
+          value={form.confirmPassword}
+          onChange={handleForm}
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
       <Link to={"/"}>
